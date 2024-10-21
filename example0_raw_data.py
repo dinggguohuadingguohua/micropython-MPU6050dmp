9f9e267349aee81e81b47379835be16df9032dd2
yanhuafeng <yanhuafeng100@gmail.com>
2024-10-22 01:45:14 +0800

    Update example0_raw_data.py

--- example0_raw_data.py
+++ example0_raw_data.py
@@ -6,6 +6,7 @@ by Jeff Rowberg Geir Istad  2015
 2 - MPU6050 Python I2C Class 
 by Majid Alekasir 2024
 ============================================
+MIT license
 Copyright (c) 2024 yanhuafeng
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
